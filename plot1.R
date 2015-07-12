432b2d78d55c71bbd8d07d8dd5817a835aeb1fc9
Joiey Seeley <joiey.seeley@gmail.com>
2015-07-11 20:37:27 -0500

    Last little bit of cleanup before submission.  Also added the ability to unzip the data file programatically so I could include it int the git repo since the unzipped version is too large.

--- plot1.R
+++ plot1.R
@@ -2,10 +2,10 @@
 setwd("/code/coursera/exploratorydataanalysis/ExData_Plotting1/")
 
 # Read in using csv with a semi-colon seperator.  Make sure stringsAsFactors is disabled
-household_power_consumption <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
+household_power_consumption <- read.csv(unz("exdata-data-household_power_consumption.zip", "household_power_consumption.txt"), sep = ";", stringsAsFactors = FALSE)
 
 # subset out just the first 2 days of Feb 2007
-begin_of_feb <- d[d$Date == "1/2/2007" | d$Date == "2/2/2007" ,]
+begin_of_feb <- household_power_consumption[household_power_consumption$Date == "1/2/2007" | household_power_consumption$Date == "2/2/2007" ,]
 
 # Display the histogram then copy it to a png file.
 hist(as.numeric(begin_of_feb$Global_active_power), xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
--- plot2.R
+++ plot2.R
@@ -1,10 +1,8 @@
-if (!"lubridate" %in% installed.packages()){ install.packages("lubridate")} 
-
 # Set the working directory to where I've download the data file
 setwd("/code/coursera/exploratorydataanalysis/ExData_Plotting1/")
 
 # Read in using csv with a semi-colon seperator.  Make sure stringsAsFactors is disabled
-household_power_consumption <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
+household_power_consumption <- read.csv(unz("exdata-data-household_power_consumption.zip", "household_power_consumption.txt"), sep = ";", stringsAsFactors = FALSE)
 
 # subset out just the first 2 days of Feb 2007
 begin_of_feb <- household_power_consumption[household_power_consumption$Date == "1/2/2007" | household_power_consumption$Date == "2/2/2007" ,]
